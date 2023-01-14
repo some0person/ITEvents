4d3ed10a0d664f27dccbae794710f31882d3af0a
some0person <some0person@github.com>
2023-01-14 21:15:50 +0300

    Added mosh (mos olimpiada ru) spider

--- Scrapy/Scrapy/spiders/olimpiadaru.py
+++ Scrapy/Scrapy/spiders/olimpiadaru.py
@@ -3,20 +3,8 @@ import scrapy
 
 class Olimpiadaru(scrapy.Spider):
     name = 'olimpiadaru'
-    allowed_domains = ['mos.olimpiada.ru']
+    allowed_domains = ['olimpiada.ru']
     start_urls = ['https://olimpiada.ru/news']
     
     def parse(self, response):
-        news = response.xpath("/html/body/div/div[3]/div[1]")
-        for n in news:
-            for date in n.css("h2::text").getall():
-                names = n.css("a::text").getall(),
-                links = n.css("a::attr(href)").extract()
-                print(11111111111111111, names[0])
-                for x in range(len(names[0])):
-                    yield {"date": date,
-                           "name": names[x],
-                           "link": links[x]}
-
-
-
+        pass
--- main.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-
-
-def writeData(source, date, title, link='', description=''):
-    pass
