2ca46099278cb54d326a4edd7cdbbeaee9d879ca
some0person <109872677+some0person@users.noreply.github.com>
2023-03-25 18:20:59 +0300

    Reverse bugfix

--- Flask/database/database.py
+++ Flask/database/database.py
@@ -13,10 +13,15 @@ class News:
         self.cur = self.connection.cursor()
 
     def getNews(self, start, end, sortby="id", reverse=False, filtercell="title", filterarg=''):
-        self.cur.execute(f"SELECT * FROM (SELECT id,source,title,link,description,date,row_number() OVER (ORDER BY {sortby} ASC) AS rn FROM news WHERE {filtercell} LIKE '%{filterarg}%') sub WHERE rn>='{start}' AND rn<='{end}' ORDER By rn DESC")
+        if reverse:
+            reverse = "DESC"
+        else:
+            reverse = "ASC"
+        self.cur.execute(f"SELECT * FROM (SELECT id,source,title,link,description,date,row_number() \
+                           OVER (ORDER BY {sortby} {reverse}) AS rn FROM news WHERE {filtercell} \
+                           LIKE '%{filterarg}%') sub WHERE rn>='{start}' AND rn<='{end}' ORDER By rn DESC")
         data = [{"id": element[0], "source": element[1],
                 "title": element[2], "link": element[3],
                 "description": element[4], "date": element[4]} for element in self.cur.fetchall()]
-        if reverse:
-            return data
+
         return data[::-1]
