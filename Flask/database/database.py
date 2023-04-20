e5b0b30c2e11f358c34d7e9f23d63620b92382fa
some0person <109872677+some0person@users.noreply.github.com>
2023-04-20 16:45:44 +0300

    Function returns filtered entries count

--- Flask/database/database.py
+++ Flask/database/database.py
@@ -20,7 +20,10 @@ class News:
         self.cur.execute(f"SELECT * FROM (SELECT id,source,title,link,description,date,row_number() \
                            OVER (ORDER BY {sortby} {reverse}) AS rn FROM news WHERE {filtercell} \
                            LIKE '%{filterarg}%') sub WHERE rn>='{start}' AND rn<='{end}' ORDER By rn DESC")
+        
         data = [{"id": element[0], "source": element[1],
                 "title": element[2], "link": element[3],
-                "description": element[4], "date": element[5]} for element in self.cur.fetchall()]
-        return data[::-1]
+                "description": element[4], "date": element[5],
+                "count": element[6]} for element in self.cur.fetchall()]
+        self.cur.execute(f"SELECT COUNT(*) FROM news WHERE {filtercell} LIKE '%{filterarg}%'")
+        return [data[::-1], self.cur.fetchone()[0]]
